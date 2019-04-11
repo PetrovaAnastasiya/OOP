51633fd9ddfd08a0426c9cb2d8addb6d30c7760b
PetrovaAnastasiya <33149053+PetrovaAnastasiya@users.noreply.github.com>
2019-04-11 11:08:02 +0300

    Add files via upload

--- multmatrix/multmatrix/Test.bat
+++ multmatrix/multmatrix/Test.bat
@@ -2,10 +2,44 @@
 set PROGRAM="%~1"
 set OUTFILE="out.txt"
 
-:: Проверка на ввод только .exe
-::%PROGRAM% > %OUTFILE%  
-::fc %OUTFILE% error-elem-num.txt || goto err
+::Проверка на ввод только .exe
+%PROGRAM% > %OUTFILE%
+fc %OUTFILE% error-elem-num.txt || goto err
 
+:: Проверка на ввод большего кол-ва аргументов 
+%PROGRAM% matrix1.txt matrix2.txt matrix1.txt > %OUTFILE%
+fc %OUTFILE% error-elem-num.txt || goto err
+
+:: Проверка на ввод меньшего кол-ва аргументов 
+%PROGRAM% matrix1.txt > %OUTFILE%
+fc %OUTFILE% error-elem-num.txt || goto 
+
+:: Проверка на несуществующий файл
+%PROGRAM% matrix1.txt matrix3.txt > %OUTFILE%
+fc %OUTFILE% error-file-open.txt || goto err
+
+%PROGRAM% matrix3.txt matrix1.txt > %OUTFILE%
+fc %OUTFILE% error-file-open.txt || goto err
+
+::Проверка на два несуществующих файла
+%PROGRAM% matrix3.txt matrix4.txt > %OUTFILE%
+fc %OUTFILE% error-file-open.txt || goto err
+
+::Проверка на A * B
+%PROGRAM% matrix1.txt matrix2.txt > %OUTFILE%
+fc %OUTFILE% result1.txt || goto err
+
+::Проверка на B * A
+%PROGRAM% matrix2.txt matrix1.txt > %OUTFILE%
+fc %OUTFILE% result2.txt || goto err
+
+::Проверка на A * единичная матрица
+%PROGRAM% matrix1.txt matrixIdentity.txt > %OUTFILE%
+fc %OUTFILE% result3.txt || goto err
+
+::Проверка на единичная матрица * A
+%PROGRAM% matrixIdentity.txt matrix1.txt > %OUTFILE%
+fc %OUTFILE% result3.txt || goto err
 
 echo All tests passed
 exit /B 0
--- multmatrix/multmatrix/multmatrix.cpp
+++ multmatrix/multmatrix/multmatrix.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -10,39 +11,42 @@ const string ERR_FILE_OPEN = "Error: file not opened";
 
 const int SIZE = 3;
 
-//засунуть матрицу в стракт и передать по ссылке
+struct MyMatrix
+{
+	double matrix_struct[SIZE][SIZE];
+};
 
-bool openFile(ifstream& file, string fileName)
+int openFile(ifstream& file, string fileName)
 {
 	file.open(fileName);
 	if (file.is_open())
-		return true;
+		return 0;
 	else
 	{
-		return false;
+		return 1;
 	}
 }
 
-int printMatrix(int matrix[SIZE][SIZE])
+int printMatrix(MyMatrix& matrix)
 {
 	for (int i = 0; i < SIZE; i++)
 	{
 		for (int j = 0; j < SIZE; j++)
 		{
-			cout << matrix[i][j] << " ";
+			cout << fixed << setprecision(3) << matrix.matrix_struct[i][j] << " ";
 		}
 		cout << endl;
 	}
 	return 0;
 }
 
-int readMatrix(ifstream& file, int matrix[SIZE][SIZE])
+int readMatrix(ifstream& file, MyMatrix& matrix)
 {
 	for (int i = 0; i < SIZE; i++)
 	{
 		for (int j = 0; j < SIZE; j++)
 		{
-			file >> matrix[i][j];
+			file >> matrix.matrix_struct[i][j];
 		}
 	}
 	return 0;
@@ -60,9 +64,9 @@ int main(int argc, char* argv[])
 	const string FILE_IN_FIRST = argv[1];
 	const string FILE_IN_SECOND = argv[2];
 
-	int firstMatrix[SIZE][SIZE] = {};
-	int secondMatrix[SIZE][SIZE] = {};
-	int resultMatrix[SIZE][SIZE] = {};
+	MyMatrix firstMatrix = {};
+	MyMatrix secondMatrix = {};
+	MyMatrix resultMatrix = {};
 
 	ifstream fileFirst;
 	openFile(fileFirst, FILE_IN_FIRST);
@@ -75,25 +79,14 @@ int main(int argc, char* argv[])
 	for (int i = 0; i < SIZE; i++)
 		for (int j = 0; j < SIZE; j++)
 		{
-			resultMatrix[i][j] = 0;
+			resultMatrix.matrix_struct[i][j] = 0;
 			for (int k = 0; k < SIZE; k++)
-				resultMatrix[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+				resultMatrix.matrix_struct[i][j] += firstMatrix.matrix_struct[i][k] * secondMatrix.matrix_struct[k][j];
 		}
 
-	if ((openFile(fileFirst, FILE_IN_FIRST)) || (openFile(fileFirst, FILE_IN_FIRST)))
-		cout << ERR_FILE_OPEN << endl;
-	else
+	if ((openFile(fileFirst, FILE_IN_FIRST) == 0) && (openFile(fileSecond, FILE_IN_SECOND) == 0))
 		printMatrix(resultMatrix);
-
-		/*if (openFile(fileFirst, FILE_IN_FIRST))
-		printMatrix(firstMatrix);
 	else
-		cout << ERR_FILE_OPEN;
-
-	if (openFile(fileSecond, FILE_IN_SECOND))
-		printMatrix(secondMatrix);
-	else
-		cout << ERR_FILE_OPEN;
-	*/
-		return 0;
+		cout << ERR_FILE_OPEN << endl;
+	return 0;
 }
